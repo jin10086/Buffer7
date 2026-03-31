9afdefbcad34857c7adbbab4e2fbdc24572a44ba
jin10086 <igaojin@qq.com>
2026-03-31 17:50:47 +0800

    feat: implement npm metadata filter

---
+++ pkg/filter/npm.go
@@ -0,0 +1,59 @@
+package filter
+
+import (
+	"encoding/json"
+	"time"
+)
+
+func FilterNPM(body []byte) ([]byte, error) {
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		return body, err
+	}
+
+	times, ok := data["time"].(map[string]interface{})
+	if !ok { return body, nil }
+	versions, ok := data["versions"].(map[string]interface{})
+	if !ok { return body, nil }
+	distTags, _ := data["dist-tags"].(map[string]interface{})
+
+	var forbidden []string
+	for v, tStr := range times {
+		if v == "created" || v == "modified" { continue }
+		if !IsSafe(tStr.(string)) {
+			forbidden = append(forbidden, v)
+		}
+	}
+
+	for _, v := range forbidden {
+		delete(versions, v)
+		delete(times, v)
+	}
+
+	if distTags != nil {
+		latest, _ := distTags["latest"].(string)
+		isForbidden := false
+		for _, v := range forbidden {
+			if v == latest { isForbidden = true; break }
+		}
+		if isForbidden {
+			// Find newest safe version
+			var newestSafe string
+			var newestTime time.Time
+			for v, tStr := range times {
+				if v == "created" || v == "modified" { continue }
+				tStrVal, ok := tStr.(string)
+				if !ok { continue }
+				t, err := time.Parse(time.RFC3339, tStrVal)
+				if err != nil { continue }
+				if t.After(newestTime) {
+					newestTime = t
+					newestSafe = v
+				}
+			}
+			distTags["latest"] = newestSafe
+		}
+	}
+
+	return json.Marshal(data)
+}
---
+++ pkg/filter/npm_test.go
@@ -0,0 +1,42 @@
+package filter
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+func TestFilterNPM(t *testing.T) {
+	now := time.Now().UTC()
+	oldVer := now.AddDate(0, 0, -10).Format(time.RFC3339)
+	newVer := now.AddDate(0, 0, -2).Format(time.RFC3339)
+
+	input := map[string]interface{}{
+		"time": map[string]interface{}{
+			"1.0.0": oldVer,
+			"1.1.0": newVer,
+		},
+		"versions": map[string]interface{}{
+			"1.0.0": map[string]interface{}{},
+			"1.1.0": map[string]interface{}{},
+		},
+		"dist-tags": map[string]interface{}{
+			"latest": "1.1.0",
+		},
+	}
+
+	body, _ := json.Marshal(input)
+	filtered, _ := FilterNPM(body)
+
+	var output map[string]interface{}
+	json.Unmarshal(filtered, &output)
+
+	versions := output["versions"].(map[string]interface{})
+	if _, ok := versions["1.1.0"]; ok {
+		t.Errorf("Expected 1.1.0 to be filtered out")
+	}
+	distTags := output["dist-tags"].(map[string]interface{})
+	if distTags["latest"] != "1.0.0" {
+		t.Errorf("Expected latest to be downgraded to 1.0.0, got %v", distTags["latest"])
+	}
+}
